15961fe2563a2299bae85a341949bb4068966676
RanatoFarofa <renatosoaresgalina@gmail.com>
2020-04-23 15:08:57 -0400

    Feitas as alterações da issue # 1

--- lab2/lab2/Arquivo2.java
+++ lab2/lab2/Arquivo2.java
@@ -8,7 +8,10 @@ public class Arquivo2 {
     public static void main(String[] args) {
         DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         Date date = new Date();
-        // Imprime a data formatada
-        System.out.println("Hoje é "+dateFormat.format(date)); 
-    }
+        //Imprime a data formatada
+        System.out.println("Hoje é "+dateFormat.format(date));
+        int a = 5;
+        int b = 10;
+        System.out.println("a+b="+(a+b));
+}
 }
